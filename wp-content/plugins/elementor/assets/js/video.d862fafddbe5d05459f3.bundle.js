27e20b14f97ab3a8c49c91e913f3303066edc2eb
parker george <parkergeorge13@gmail.com>
2025-09-03 19:57:35 -0700

    updates

--- wp-content/plugins/elementor/assets/js/video.d862fafddbe5d05459f3.bundle.js
+++ wp-content/plugins/elementor/assets/js/video.d862fafddbe5d05459f3.bundle.js
@@ -1,4 +1,4 @@
-/*! elementor - v3.30.0 - 22-07-2025 */
+/*! elementor - v3.31.0 - 27-08-2025 */
 "use strict";
 (self["webpackChunkelementorFrontend"] = self["webpackChunkelementorFrontend"] || []).push([["video"],{
 
